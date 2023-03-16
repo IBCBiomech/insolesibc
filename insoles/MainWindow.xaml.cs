17a08b8d3cd76507789b0e99cee630588a8427c7
Bernat <bernatdiaz94@gmail.com>
2023-03-16 14:32:07 +0100

    SplashScreen

--- insoles/MainWindow.xaml.cs
+++ insoles/MainWindow.xaml.cs
@@ -54,6 +54,7 @@ namespace insoles
             //Transformers.transformImageHeatmap();
             //Transformers.transformImageButterfly();
             InitializeComponent();
+            Application.Current.MainWindow = this;
             virtualToolBar = new VirtualToolBar();
             fileSaver = new FileSaver.FileSaver();
             graphManager = new GraphManager();
