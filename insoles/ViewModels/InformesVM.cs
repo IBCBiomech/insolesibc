4d62ac12be3e4f12dfa7181b2225b7d62fdef775
Bernat <bernatdiaz94@gmail.com>
2023-07-03 09:44:22 +0200

    TreeView usuarios en reporting

--- insoles/ViewModels/InformesVM.cs
+++ insoles/ViewModels/InformesVM.cs
@@ -1,8 +1,29 @@
-﻿using insoles.Utilities;
+﻿using insoles.Commands;
+using insoles.Model;
+using insoles.States;
+using insoles.Utilities;
+using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace insoles.ViewModel
 {
     public class InformesVM : ViewModelBase
     {
+        private DatabaseBridge databaseBridge;
+        public ObservableCollection<Paciente> Pacientes
+        {
+            get
+            {
+                return databaseBridge.Pacientes;
+            }
+        }
+        public ObtenerPacientesCommand obtenerPacientesCommand { get; set; }
+        public CrearPacienteCommand crearPacienteCommand { get; set; }
+        public InformesVM()
+        {
+            databaseBridge = ((MainWindow)Application.Current.MainWindow).databaseBridge;
+            crearPacienteCommand = new CrearPacienteCommand(databaseBridge);
+            obtenerPacientesCommand = new ObtenerPacientesCommand(databaseBridge);
+        }
     }
 }
