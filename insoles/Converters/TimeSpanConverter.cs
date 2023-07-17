943e7da0093cd715c70f41ffcbe6fd666fa9caf2
Bernat <bernatdiaz94@gmail.com>
2023-07-17 11:18:47 +0200

    no se mostraba el tiempo en el timer

---
+++ insoles/Converters/TimeSpanConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace insoles.Converter
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class TimeSpanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double seconds)
+            {
+                TimeSpan timeSpan = TimeSpan.FromSeconds(seconds);
+                return timeSpan.ToString(@"hh\:mm\:ss\:fff");
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
